e67553cb65fd8453a67952ffa4f9c8486f28f091
Azhagu <azhagukarthikeyanb@gmail.com>
2025-02-07 19:41:30 +0530

    Added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api/*": "https://task-manager-3-um38.onrender.com",
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     baseURl: "https://task-manager-3-um38.onrender.com",
+  //   },
+  // },
 
   plugins: [react(), tailwindcss()],
 });
