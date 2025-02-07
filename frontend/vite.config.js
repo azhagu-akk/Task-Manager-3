0a1e091fc79f10327056eec26e3b0b70a4fcbc82
Azhagu <azhagukarthikeyanb@gmail.com>
2025-02-07 08:59:14 +0530

    Added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://task-manager-3-aziy.onrender.com",
+      "/api": "https://task-manager-3-um38.onrender.com",
     },
   },
 
